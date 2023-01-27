dbf949a27bcab76a41f4224016fba8485d151d9f
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-01-27 16:58:44 +0100

    Add module for storing all projects

---
+++ src/modules/allProjects.js
@@ -0,0 +1,22 @@
+import createProject from "./project";
+
+export default function projectsContainer() {
+  const homeTab = new createProject("Home");
+  const todayTab = new createProject("Today");
+  const weekTab = new createProject("Week");
+
+  const gymTab = new createProject("Gym");
+  const bookTab = new createProject("Book");
+
+  let projects = [homeTab, todayTab, weekTab, gymTab, bookTab];
+
+  function add(project) {
+    projects.push(project);
+  }
+
+  function getProjects() {
+    return projects
+  }
+
+  return { getProjects, add };
+}
