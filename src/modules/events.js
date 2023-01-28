58bd23a64b5882f36660e02a6222e14564dd51f5
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-01-28 20:36:03 +0100

    Add modal event listeners for tab switching logic

--- src/modules/events.js
+++ src/modules/events.js
@@ -1,28 +1,37 @@
 import { uiController } from "./ui";
 
 export const eventListeners = (function () {
-  const todoForm = document.getElementById("todoForm");
-
   document.addEventListener("click", (e) => {
-    if (e.target.classList.contains("add-todo-btn"))
+    if (e.target.classList.contains("add-btn")) {
+      uiController.renderModal();
       uiController.toggleAddModal();
-
-    if (e.target.classList.contains("add-project-btn")) {
-      uiController.toggleAddModal();
-      uiController.renderProject();
     }
 
     if (e.target.classList.contains("tab")) {
       if (e.target.classList.contains("active")) return;
-      uiController.switchActiveStatus(e.target.classList);
+
+      const tabs = document.querySelectorAll(".tab");
+      uiController.switchActiveStatus(e.target, tabs);
       uiController.renderTabContainer();
     }
   });
 
-  todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    uiController.createTodoItem();
-    uiController.clearInputs();
-    uiController.toggleAddModal();
+  document.addEventListener("submit", (e) => {
+    if (e.target.classList.contains("todo-form")) {
+      e.preventDefault();
+      uiController.renderTodoItem();
+      uiController.clearInputs();
+      uiController.toggleAddModal();
+      uiController.renderTabContainer();
+    }
   });
+
+  const navModalChildren = document.querySelector(".modal-nav").children;
+  const navModalTab = document.querySelectorAll(".modal-tab");
+  for (let child of navModalChildren) {
+    child.addEventListener(
+      "click",
+      () => uiController.switchActiveStatus(child, navModalTab)
+    );
+  }
 })();
