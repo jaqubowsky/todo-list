9c35bbeda25739ec24880fb721059017f6e43615
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-01-31 15:28:58 +0100

    Clean code

--- src/modules/events.js
+++ src/modules/events.js
@@ -1,52 +1,61 @@
 import { uiController } from "./ui";
 
 export const eventListeners = (function () {
-  const formContainer = document.getElementById("formContainer");
+  const formsContainer = document.querySelectorAll("form");
+  const exitBtn = document.getElementById("exitBtn");
+  const todoTab = document.querySelector("[data-todo-tab]");
+  const projectTab = document.querySelector("[data-project-tab]");
+  const formTodoContainer = document.getElementById("formTodoContainer");
+  const formProjectContainer = document.getElementById("formProjectContainer");
+  const navModalTab = document.querySelectorAll(".modal-tab");
 
   document.addEventListener("click", (e) => {
     if (
       e.target.classList.contains("add-btn") ||
       e.target.classList.contains("exit-btn")
     ) {
-      uiController.toggleAddModal();
+      uiController.toggleModalVisibility();
     }
 
     if (e.target.classList.contains("tab")) {
-      if (e.target.classList.contains("active")) return;
-
       const tabs = document.querySelectorAll(".tab");
+
       uiController.switchActiveStatus(e.target, tabs);
-      uiController.renderTabContainer();
+      uiController.renderTodos();
     }
 
-    if (e.target.classList.contains("modal-tab")) {
-      uiController.renderModal(e);
+    if (e.target === todoTab) {
+      uiController.renderModal(formTodoContainer);
     }
 
-    if (e.target.classList.contains("exit-btn")) {
+    if (e.target === projectTab) {
+      uiController.renderModal(formProjectContainer);
+    }
+
+    if (e.target === exitBtn) {
+      document.querySelector(".todo-modal").classList.add("hidden");
     }
   });
 
-  formContainer.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (e.submitter.classList.contains("add-project-btn")) {
-      uiController.renderProject();
-    }
-    if (e.submitter.classList.contains("add-todo-btn")) {
-      uiController.renderTodoItem();
-    }
+  formsContainer.forEach((form) => {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      if (e.submitter.classList.contains("add-project-btn")) {
+        uiController.renderProject();
+      }
+      if (e.submitter.classList.contains("add-todo-btn")) {
+        uiController.addTodoItem();
+      }
 
-    uiController.renderTabContainer();
-    uiController.toggleAddModal();
-    uiController.clearInputs();
+      uiController.renderTodos();
+      uiController.toggleModalVisibility();
+      uiController.clearInputs();
+    });
   });
 
-  const navModalChildren = document.querySelector(".modal-nav").children;
-  const navModalTab = document.querySelectorAll(".modal-tab");
-
-  for (let child of navModalChildren) {
+  navModalTab.forEach((child) => {
     child.addEventListener("click", () =>
       uiController.switchActiveStatus(child, navModalTab)
     );
-  }
+  });
 })();
