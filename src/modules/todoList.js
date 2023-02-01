ecdcdb6948c4d7ddd2d1fb466f9458e9138ca624
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-02-01 23:35:56 +0100

    Change module name

---
+++ src/modules/todoList.js
@@ -0,0 +1,30 @@
+import uuid4 from "uuid4";
+
+export default function createProject(name) {
+  const title = name;
+
+  const id = uuid4();
+  let todos = [];
+
+  function add(todo) {
+    todos.push(todo);
+  }
+
+  function deleteTodo(todo) {
+    todos = todos.filter((toDoItem) => toDoItem.getId() !== todo.dataset.id);
+  }
+
+  function getTodos() {
+    return Object.freeze([...todos]);
+  }
+
+  function getTitle() {
+    return title;
+  }
+
+  function getId() {
+    return Object.freeze(id);
+  }
+
+  return Object.freeze({ getTitle, add, deleteTodo, getTodos, getId });
+}
